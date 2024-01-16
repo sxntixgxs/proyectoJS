4a2e59298b3c790516b2f01a361f42fbf5b3582b
Santiago Sandoval <sandovalbgasantiago@gmail.com>
2024-01-15 20:55:07 -0500

    sano

--- PRIME/js/registro.js
+++ PRIME/js/registro.js
@@ -1,8 +1,8 @@
-import { checkUsuario } from "./checkUsuario";
-import { habilitaRegistro } from "./habilitarRegistro";
-import { registrarUser } from "./registrarUser";
-import { registerApproved } from "./registerApproved";
-import { registerRechazo } from "./registerRechazo";
+import { checkUsuario } from "./checkUsuario.js";
+import { habilitaRegistro } from "./habilitarRegistro.js";
+import { registrarUser } from "./registrarUser.js";
+import { registerApproved } from "./registerApproved.js";
+import { registerRechazo } from "./registerRechazo.js";
 
 const dont_account = document.getElementById("dont_account");
 const boton = document.getElementById("register_send");
