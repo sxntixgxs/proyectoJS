ec99f30f54a3df927eddffaee3914953883e2169
Santiago Sandoval Torres <145622597+sxntixgxs@users.noreply.github.com>
2024-01-15 19:42:01 -0500

    sanandreas

--- PRIME/js/checkUsuario.js
+++ PRIME/js/checkUsuario.js
@@ -1,7 +1,7 @@
 //aqui estoy asumiendo que la db va a estar montada con un json server, entonces voy a acceder a ella de esa forma para validar los datos de inicio de sesion
 
 export async function checkUsuario(user,clave){
-    const respuesta = await fetch(`http://localhost:3000/users?nickname=${user}&pass=${clave}`, {
+    const respuesta = await fetch(`https://probable-fishstick-r4g5w5wj9q7wcqv4-3000.app.github.dev/users?nickname=${user}&pass=${clave}`, {
         method: 'GET',
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
