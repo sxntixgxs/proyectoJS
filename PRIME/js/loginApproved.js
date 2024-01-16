c0bc338d35c0bca709b32a8a6ed9eed39dabf7fb
SantiagoSandoval <sandovalbgasantiago@gmail.com>
2024-01-16 15:55:03 -0500

    sanop

--- PRIME/js/loginApproved.js
+++ PRIME/js/loginApproved.js
@@ -1,9 +1,11 @@
 export function loginApproved(){
     let nickname = document.getElementById("nickname");
     let password = document.getElementById("pass");
-    let login = document.getElementById("login_section")
+    let login = document.getElementById("login_section");
+    let register = document.getElementById("register_section");
     nickname.classList.add("correctLogin");
     password.classList.add("correctLogin");
-    setTimeout(()=>{login.style.visibility = "hidden";login.style.display="none";},1000);
+    setTimeout(()=>{login.style.visibility = "hidden";login.style.display="none";
+                    register.style.visibility = "hidden"},1000);
     
 }
