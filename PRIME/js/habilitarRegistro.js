cc69e9a32709c2607d07835fb58df5f854942e1f
Santiago Sandoval <sandovalbgasantiago@gmail.com>
2024-01-15 21:05:25 -0500

    snao

--- PRIME/js/habilitarRegistro.js
+++ PRIME/js/habilitarRegistro.js
@@ -1,5 +1,5 @@
 
 export function habilitaRegistro(){   
     const register_section = document.getElementById("register_section");
-    register_section.style.visibility = visible;
+    register_section.style.visibility = "visible";
 }
