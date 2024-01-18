4097696e7ff15b9c8fd279ce1009bb3c557a41e3
Santiago Sandoval Torres <145622597+sxntixgxs@users.noreply.github.com>
2024-01-18 00:53:52 -0500

    meh

--- PRIME/js/generarChart.js
+++ PRIME/js/generarChart.js
@@ -1,43 +1,53 @@
 
-import { albumVotos } from "./valoresGraph.js"
+import { albumVotos } from "./valoresGraph.js";
+let myChart;
+async function iniciarGraph(){
     let datos= await albumVotos();
     console.log("estos son los datos",datos)
+
     var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+    myChart = new Chart(ctx, {        
         type: 'bar',
         data: {
-            labels: datos[0],
-            datasets: [{
-                label: 'USERS VOTE',
-                data: datos[1],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 99, 132, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(255, 99, 132, 1)'
-                ],
-                borderWidth: 1
-            }]
+        labels: datos[0],
+        datasets: [{
+            label: 'USERS VOTE',
+            data: datos[1],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
         },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            },
-            responsive: true,
-            maintainAspectRatio: false
-        }
-    });
+        responsive: true,
+        maintainAspectRatio: false
+    }
+});
+}
+iniciarGraph();
+
+
+
+ 
+
