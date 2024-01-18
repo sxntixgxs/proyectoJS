6bf1d68f2b995777cdd53c364e202008ab71eb82
Santiago Sandoval <sandovalbgasantiago@gmail.com>
2024-01-17 19:01:49 -0500

    sano

--- PRIME/js/habilitarVotacion.js
+++ PRIME/js/habilitarVotacion.js
@@ -8,7 +8,7 @@ export default async function generarTarjeta() {
         const img = document.createElement("img");
         const titl = document.createElement("h2");
         const desc = document.createElement("p");
-        const btn_stats = document.createElement("button");
+        const btn_stats = document.createElement("a");
         const btn_vote = document.createElement("button");
 
         tarjeta.classList.add("card");
@@ -19,6 +19,7 @@ export default async function generarTarjeta() {
         titl.classList.add("titleAlbum");
         btn_stats.classList.add("btn_stats");
         btn_stats.id = "btn_stats";
+        btn_stats.href = "#myChart";
         btn_stats.textContent = "SHOW CHARTS";
         btn_vote.classList.add("btn_vote");
         btn_vote.id = `btn_vote${i}`;
